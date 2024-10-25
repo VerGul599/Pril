5983bfc82353739c6e9452e7127bfdc0b67cbf46
Вероника Гуляева <Veronikaguylaeva@yandex.ru>
2024-10-25 18:37:30 +0500

    Добавьте файлы проекта.

---
+++ Card.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "Card.hpp"
+
+CardGame::Suit CardGame::Card::GetSuit() const
+{
+    return Suit();
+}
+
+CardGame::Ranks CardGame::Card::GetRank() const
+{
+    return Ranks();
+}
